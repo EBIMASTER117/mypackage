bfe0d7962446ea7213004a044a5afb1155cd643f
Ebrahim Noormahomed <ebimaster117@gmail.com>
2019-03-19 10:20:35 +0200

    new package commit

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='mypackage',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/EBIMASTER117/mypackage.git',
+    url='https://github.com/EBIMASTER117/mypackage',
     author='EBIMASTER117',
     author_email='ebimaster117@gmail.com'
 )
